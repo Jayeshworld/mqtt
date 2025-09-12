af708b1d575ae2aac3884902597da1c3e40b00be
Jayeshworld <githubjd2025@gmail.com>
2025-09-12 18:16:50 +0530

    Added migrate route to run migrations from browser

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -4,18 +4,35 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
 
     public function login(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+            'fcm_token' => 'sometimes|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation errors',
+
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+
         $credentials = $request->only('email', 'password');
 
 
         if (auth()->attempt($credentials)) {
             $user = auth()->user();
-            if($request->has('fcm_token')){
+            if ($request->has('fcm_token')) {
                 $user->fcm_token = $request->fcm_token;
                 $user->save();
             }
@@ -23,13 +40,14 @@ class LoginController extends Controller
             $token = $user->createToken('api-token')->plainTextToken;
 
             return response()->json([
+                'status' => true,
+                'message' => 'User logged in successfully',
                 'access_token' => $token,
-                'token_type' => 'Bearer',
                 'user' => $user,
             ]);
         }
 
-        return response()->json(['message' => 'Invalid credentials'], 401);
+        return response()->json(['message' => 'Invalid credentials'], 400);
     }
 
     public function logout(Request $request)
@@ -38,7 +56,4 @@ class LoginController extends Controller
 
         return response()->json(['message' => 'Logged out successfully']);
     }
-
-
-    
 }
