189d8c612d9dc9e19ac138af77ecf187b9379302
Jayeshworld <githubjd2025@gmail.com>
2025-09-12 18:18:46 +0530

    Added migrate route to run migrations from browser

--- app/Http/Controllers/Api/ElevatorController.php
+++ app/Http/Controllers/Api/ElevatorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Elevator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ElevatorController extends Controller
 {
@@ -17,6 +18,7 @@ class ElevatorController extends Controller
 
     public function notification(Request $request)
     {
+        Log::info('Notification received', $request->all());
         // Logic to handle notification
         return response()->json(['message' => 'Notification received']);
     }
