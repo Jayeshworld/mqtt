dba462c54fc1e322c4342aeaf9abd6158557dd07
Jayeshworld <githubjd2025@gmail.com>
2025-09-14 18:48:59 +0530

    Added migrate route to run migrations from browser

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,13 @@ use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::post('login', [LoginController::class, 'login']);
 
 Route::get('elevators/maacid', [ElevatorController::class, 'maacid']);
 Route::post('elevators/notification', [ElevatorController::class, 'notification']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [LoginController::class, 'getUser']);
+});
 
 
